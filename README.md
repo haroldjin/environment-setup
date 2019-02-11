e064991df9a25ee26c46f1610fc7a1c88ea520e1
Harold Jin <juhao0707@gmail.com>
2019-02-10 20:08:31 -0500

    upgrade setup.sh script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # environment-setup
+1. How to setup
+    * Run `sh setup.sh` to start environment setup script.
+
+## Limitations
+1. Currently, it only supports Mac OS X for setting up brew and dependencies.
+2. Linux will support setting up bash profile and vim profile.
--- setup.sh
+++ setup.sh
@@ -1,95 +1,148 @@
-#!/bin/sh
+#prompt_select_read!/bin/sh
 
 # constants
-cNone='\033[00m'
-cRed='\033[01;31m'
-cGreen='\033[01;32m'
-cYellow='\033[01;33m'
-cPurple='\033[01;35m'
-cCyan='\033[01;36m'
-cWhite='\033[01;37m'
-cBold='\033[1m'
-cUnderline='\033[4m'
-OS_nAME=`uname -s`
+STYLE_NONE='\033[00m'
+COLOR_RED='\033[01;31m'
+COLOR_GREEN='\033[01;32m'
+COLOR_YELLOW='\033[01;33m'
+COLOR_PURPLE='\033[01;35m'
+COLOR_CYAN='\033[01;36m'
+COLOR_WHITE='\033[01;37m'
+FONT_BOLD='\033[1m'
+FONT_UNDERLINE='\033[4m'
+OS_NAME=`uname -s`
+VUNDLE_PATH='https://github.com/VundleVim/Vundle.vim.git'
+
+# Prompts
+prompt_bold(){
+    echo "${STYLE_NONE} ${FONT_BOLD}$1${STYLE_NONE}"
+}
+prompt_error(){
+    echo "${COLOR_RED}$1${STYLE_NONE}"
+}
+
+prompt_info(){
+    echo "${COLOR_GREEN}$1${STYLE_NONE}"
+}
+
+prompt_select_print(){
+    echo "${COLOR_PURPLE}$1${STYLE_NONE}${COLOR_GREEN} $2${STYLE_NONE}"
+}
+prompt_select(){
+    echo "${COLOR_PURPLE}$1${STYLE_NONE}${COLOR_GREEN} [y/n]${STYLE_NONE}"
+    read isYes
+    if [ "$isYes" == "y" ]; then
+        return 1
+    else
+        return 0
+    fi
+}
 
 # functions
 command_exists () {
     type "$1" &> /dev/null ;
 }
 
-installedHomeBrew=false
-
-# Flow
-if [ "$OS_nAME" == "Darwin" ]; then
-    echo "Do you want to set up ${cBold}Homebrew?${cNone}[y/n]"
-    read isSetupHomebrew
-    if [ "$isSetupHomebrew" == "y" ]; then
-        echo setting up homebrew
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        if [ $? -eq 0 ]; then
-            echo brew set up successfully
-            installedHomeBrew=true
-        else
-            echo brew set up failed, continuing setting up the environment
-        fi
-    else
-        echo skipping setting up homebrew
-    fi
-elif [ "$OS_nAME" == "Linux" ]; then
-    echo linux
-    # Linux specific setups
-else
-    exit 1
-fi
-
-if [ "$installedHomeBrew" = true ]; then
-    declare -a commandsToInstall=("python3" "nvm", "yarn", "npm")
+homerbew_setup(){
+    declare -a commandsToInstall=("python3" "nvm" "yarn" "npm")
 
     for command in "${commandsToInstall[@]}"; do
         command_exists "$command"
         if [ $? -eq 1 ]; then
-            echo command $command doesn\'t exist do you want to install[y/n]?
-            read isInstall
-            if [ "$isInstall" == "y" ]; then
-                echo installing command $command
+            prompt_select "command $command doesn't exist do you want to install"
+            if [ "$?" = 1 ]; then
+                prompt_info "installing command $command"
                 brew install $command
             fi
         fi
     done
 
-    echo "Do you want to install ${cBold}${cYellow}vim with python3?${cNone}[y/n]"
-    read isSetupVim
-
-    if [ "$isSetupVim" == "y" ]; then
-        echo Installing vim with python3 using brew
-        brew install vim --with-python3
+    prompt_select_print "Do you want to install `prompt_bold "vim"`" "[m/v/n]${STYLE_NONE} - m for macvim, v for vim, n to skip"
+    read vimSelect
+    if [  "$vimSelect" == 'v' ]; then
+        prompt_info "Installing `prompt_bold "vim with python3"` using brew"
+        brew install vim
         if [ $? eq 0 ]; then
-            echo vim installed successful
-        else
-            echo error installing vim
-            exit 1
+            prompt_info "vim installed successful"
+        fi
+    elif [ "$vimSelect" == 'm' ]; then
+        prompt_info "Installing `prompt_bold "macVim"` using brew"
+        brew install macvim
+        if [ $? eq 0 ]; then
+            prompt_info "macVim installed successful"
         fi
     else
-        echo skipping installing vim with python3
+        prompt_info "skipping vim installation"
     fi
-fi
+}
 
-echo setting up bash profile, git prompt \& completion
-ln -sfn `pwd`/.bashrc ~/.bash_profile
-if [ $? -eq 0 ]; then
-    echo bash set up successful
+setup_bash(){
+    prompt_info "setting up bash profile"
+    ln -sfn `pwd`/.bashrc ~/.bash_profile
+    if [ $? -eq 0 ]; then
+        prompt_info "bash set up successful"
+    else
+        prompt_error "error setting up bash profile"
+        exit 1
+    fi
+    echo
+}
+
+
+setup_vim(){
+    prompt_info "setting up vimrc and vim libraries"
+    ln -sfn `pwd`/vim/vimrc ~/.vimrc && ln -sfn `pwd`/vim ~/.vim
+    if [ $? -eq 0 ]; then
+        prompt_info "vim folder setup successful"
+    else
+        prompt_info "error setting up vim"
+        exit 1
+    fi
+
+    prompt_info "cloning vundle to vim directory"
+    if [ ! -d `pwd`/vim/bundle/Vundle.vim ]; then
+        git clone ${VUNDLE_PATH} `pwd`/vim/bundle/Vundle.vim
+    fi
+
+    prompt_info "install plugins for vim"
+    vim +PluginInstall +qall
+
+    prompt_select "do you want to set up YouCompleteMe?"
+    if [ $? == 1 ]; then
+        python `pwd`/vim/bundle/YouCompleteMe/install.py --all
+    fi
+    echo
+}
+
+setup_mac(){
+    prompt_select "Do you want to set up `prompt_bold "Homebrew"`"
+    if [ $? == 1 ]; then
+        prompt_info "setting up homebrew"
+        # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        if [ $? -eq 0 ]; then
+            prompt_info "brew set up successfully"
+            homerbew_setup
+        else
+            prompt_error "brew set up failed, continuing setting up the environment"
+        fi
+    else
+        prompt_error "skipping setting up homebrew"
+    fi
+}
+
+# MAIN
+if [ "$OS_NAME" == "Darwin" ]; then
+    setup_mac
+elif [ "$OS_NAME" == "Linux" ]; then
+    prompt_error "$OS_NAME is not yet implemented for OS specific packages"
+    # Linux specific setups
 else
-    echo error setting up bash profile
+    prompt_error "$OS_NAME is not yet implemented for OS specific packages and for bash and vim"
     exit 1
 fi
 
-echo setting up vimrc and vim libraries
-ln -sfn `pwd`/vim/vimrc ~/.vimrc && ln -sfn `pwd`/vim ~/.vim
-if [ $? -eq 0 ]; then
-    echo vim setup successful
-else
-    echo error setting up vim
-    exit 1
-fi
+# Setup vim now
+setup_bash
+setup_vim
 
-echo "${cGreen}setup completed!${cNone}"
+prompt_info "setup completed!"
