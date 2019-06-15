f71e3ebfe16941bf6234fee086a505e9d59ec05d
Harold Jin <juhao0707@gmail.com>
2019-06-15 15:43:03 -0700

    configure setup.sh to setup zsh

--- setup.sh
+++ setup.sh
@@ -1,35 +1,54 @@
-#prompt_select_read!/bin/sh
+#!/bin/bash
 
-# constants
-STYLE_NONE='\033[00m'
-COLOR_RED='\033[01;31m'
-COLOR_GREEN='\033[01;32m'
-COLOR_YELLOW='\033[01;33m'
-COLOR_PURPLE='\033[01;35m'
-COLOR_CYAN='\033[01;36m'
-COLOR_WHITE='\033[01;37m'
-FONT_BOLD='\033[1m'
-FONT_UNDERLINE='\033[4m'
+if which tput >/dev/null 2>&1; then
+    colors=$(tput colors)
+fi
+
+if [ -t 1 ] && [ -n "$colors" ] && [ "$colors" -ge 8 ]; then
+    # constants
+    STYLE_NONE='\033[00m'
+    COLOR_RED='\033[01;31m'
+    COLOR_GREEN='\033[01;32m'
+    COLOR_YELLOW='\033[01;33m'
+    COLOR_PURPLE='\033[01;35m'
+    COLOR_CYAN='\033[01;36m'
+    COLOR_WHITE='\033[01;37m'
+    FONT_BOLD='\033[1m'
+    FONT_UNDERLINE='\033[4m'
+else
+    STYLE_NONE=''
+    COLOR_RED=''
+    COLOR_GREEN=''
+    COLOR_YELLOW=''
+    COLOR_PURPLE=''
+    COLOR_CYAN=''
+    COLOR_WHITE=''
+    FONT_BOLD=''
+    FONT_UNDERLINE=''
+fi
+
 OS_NAME=`uname -s`
 VUNDLE_PATH='https://github.com/VundleVim/Vundle.vim.git'
 
-# Prompts
+# Prompts with enable backslash interpretation
 prompt_bold(){
-    echo "${STYLE_NONE} ${FONT_BOLD}$1${STYLE_NONE}"
+    echo -e "${STYLE_NONE} ${FONT_BOLD}$1${STYLE_NONE}"
 }
+
 prompt_error(){
-    echo "${COLOR_RED}$1${STYLE_NONE}"
+    echo -e "${COLOR_RED}$1${STYLE_NONE}"
 }
 
 prompt_info(){
-    echo "${COLOR_GREEN}$1${STYLE_NONE}"
+    echo -e "${COLOR_GREEN}$1${STYLE_NONE}"
 }
 
 prompt_select_print(){
-    echo "${COLOR_PURPLE}$1${STYLE_NONE}${COLOR_GREEN} $2${STYLE_NONE}"
+    echo -e "${COLOR_PURPLE}$1${STYLE_NONE}${COLOR_GREEN} $2${STYLE_NONE}"
 }
-prompt_select(){
-    echo "${COLOR_PURPLE}$1${STYLE_NONE}${COLOR_GREEN} [y/n]${STYLE_NONE}"
+
+prompt_select_return(){
+    echo -e "${COLOR_PURPLE}$1${STYLE_NONE}${COLOR_GREEN} [y/n]${STYLE_NONE}"
     read isYes
     if [ "$isYes" == "y" ]; then
         return 1
@@ -49,7 +68,7 @@ homerbew_setup(){
     for command in "${commandsToInstall[@]}"; do
         command_exists "$command"
         if [ $? -eq 1 ]; then
-            prompt_select "command $command doesn't exist do you want to install"
+            prompt_select_return "command $command doesn't exist do you want to install"
             if [ "$?" = 1 ]; then
                 prompt_info "installing command $command"
                 brew install $command
@@ -78,6 +97,14 @@ homerbew_setup(){
 
 setup_bash(){
     prompt_info "setting up bash profile"
+    if [ -z $1 ] || [ $1 != "bash" ]; then
+        prompt_select_return "Shell $1 is not bash, do you want to change your default shell to bash"
+        if [ $? == 1 ]; then
+            prompt_info "changing shell from $1 to bash"
+            chsh -s $(which bash)
+        fi
+    fi
+
     ln -sfn `pwd`/.bashrc ~/.bash_profile
     if [ $? -eq 0 ]; then
         prompt_info "bash set up successful"
@@ -88,8 +115,32 @@ setup_bash(){
     echo
 }
 
+setup_zsh(){
+    prompt_info "setting up zsh profile"
+    if [ -z $1 ] || [ $1 != "zsh" ]; then
+        prompt_select_return "Shell $1 is not zsh, do you want to change your default shell to zsh"
+        if [ $? == 1 ]; then
+            prompt_info "changing shell from $1 to zsh"
+            chsh -s $(which zsh)
+        fi
+    fi
+
+    ln -sfn `pwd`/.zshrc ~/.zshrc
+    if [ $? -eq 0 ]; then
+        prompt_info "zsh set up successful"
+    else
+        prompt_error "error setting up zsh profile"
+        exit 1
+    fi
+    echo
+}
+
 
 setup_vim(){
+    prompt_select_return "Do you want to set up vim?"
+    if [ $? == 0 ]; then
+        return 0
+    fi
     prompt_info "setting up vimrc and vim libraries"
     ln -sfn `pwd`/vim/vimrc ~/.vimrc && ln -sfn `pwd`/vim ~/.vim
     if [ $? -eq 0 ]; then
@@ -107,7 +158,7 @@ setup_vim(){
     prompt_info "install plugins for vim"
     vim +PluginInstall +qall
 
-    prompt_select "do you want to set up YouCompleteMe?"
+    prompt_select_return "do you want to set up YouCompleteMe?"
     if [ $? == 1 ]; then
         python `pwd`/vim/bundle/YouCompleteMe/install.py --all
     fi
@@ -115,7 +166,7 @@ setup_vim(){
 }
 
 setup_mac(){
-    prompt_select "Do you want to set up `prompt_bold "Homebrew"`"
+    prompt_select_return "Do you want to set up `prompt_bold "Homebrew"`"
     if [ $? == 1 ]; then
         prompt_info "setting up homebrew"
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -130,7 +181,21 @@ setup_mac(){
     fi
 }
 
-# MAIN
+setup_shell(){
+    prompt_select_print "Do you want to setup `prompt_bold "bash or zsh"`" "[b/z/n]${STYLE_NONE} - b for bash, z for zsh, n to skip"
+    read shellSelect
+    cur_shell=$(basename $SHELL)
+    if [  "$shellSelect" == 'b' ]; then
+        setup_bash $cur_shell
+    elif [ "$shellSelect" == 'z' ]; then
+        setup_zsh $cur_shell
+    else
+        prompt_info "skipping shell installation"
+    fi
+}
+
+# ======= MAIN =======
+## Setup dependencies
 if [ "$OS_NAME" == "Darwin" ]; then
     setup_mac
 elif [ "$OS_NAME" == "Linux" ]; then
@@ -141,8 +206,10 @@ else
     exit 1
 fi
 
-# Setup vim now
-setup_bash
+## Setup she
+setup_shell
+
+## Setup vim
 setup_vim
 
 prompt_info "setup completed!"
